9cd4502f63fc0336489aca59cdcaf0121e4dcdaa
iqbalnur21 <95479268+iqbalnur21@users.noreply.github.com>
2021-12-12 06:06:13 +0700

    Update index.js

--- src/config/FIREBASE/index.js
+++ src/config/FIREBASE/index.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBjr-zB5MIQmRqyta6x8Bqaz72QrUw1gmI",
-    authDomain: "onlinelaundry-1b7fc.firebaseapp.com",
-    databaseURL: "https://onlinelaundry-1b7fc-default-rtdb.firebaseio.com",
-    projectId: "onlinelaundry-1b7fc",
-    storageBucket: "onlinelaundry-1b7fc.appspot.com",
-    messagingSenderId: "619244904671",
-    appId: "1:619244904671:web:3df40d6a9be3d37308096a",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
 
 const FIREBASE = firebase;
